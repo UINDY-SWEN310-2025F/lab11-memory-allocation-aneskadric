93cc13af1f6ea35659498267188e5983126dc4cf
Anes Kadric <kadrica@uindy.edu>
2025-11-21 10:31:12 -0500

    lab 11 AK

--- answer/mem_monitor.sh
+++ answer/mem_monitor.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# mem_monitor.sh - show memory and swap usage in human-readable format every 2 seconds
-
-echo "Showing memory usage every 2 seconds (Ctrl+C to stop)"
-
-# Run `free` in human-readable mode (-h) every 2 seconds
-# Use free's built-in sampling (-s) instead of writing our own loop
 free -h -s 2
